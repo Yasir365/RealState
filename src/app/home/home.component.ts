6c15e04a2219db721bab17064c50d70d4ae04568
yasirsaleemvws <126661150+Yasir365@users.noreply.github.com>
2024-12-05 17:32:28 +0500

    content added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../layout/header/header.component';
+import { FooterComponent } from '../layout/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
