fc17add60192f0e51a9d8b5962f5203be06b2c81
Yasir Saleem <126661150+Yasir365@users.noreply.github.com>
2024-12-10 09:23:31 +0500

    set content

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
   },
   {
+    path: 'index',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
     path: 'about-us',
     loadComponent: () => import('./about-us/about-us.component').then(m => m.AboutUsComponent)
   },
---
+++ src/app/shared/contact-form/contact-form.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.scss']
+})
+export class ContactFormComponent {
+  contactForm: FormGroup;
+  serviceID = 'service_i4ydub9';  
+  templateID = 'template_504xfje';
+  publicKey = 'UMljUzlyM80ldsi4x';
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      emailjs
+      .send(this.serviceID, this.templateID, this.contactForm.getRawValue(), this.publicKey)
+      .then(
+        (response) => {
+          console.log('Email sent successfully!', response.status, response.text);
+          this.contactForm.reset();
+          alert('Email sent successfully!');
+        },
+        (error) => {
+          console.error('Failed to send email.', error);
+          alert('Failed to send email. Please try again later.');
+        }
+      );
+    }
+  }
+}
