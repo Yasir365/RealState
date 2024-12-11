c6454e5ccce232063773c0cea8a8ec7efca0fb5e
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-12-11 16:31:05 +0500

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactFormComponent } from '../shared/contact-form/contact-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,ContactFormComponent],
+  imports: [CommonModule, ContactFormComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
