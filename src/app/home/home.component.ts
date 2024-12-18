bc6086e977c05411abddba2e4dc0b732e4a811ff
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-12-18 12:34:05 +0500

    about section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -66,4 +66,32 @@ export class HomeComponent {
     },
   ]
 
+  factions = [
+    {
+      name: 'What is REAL?',
+      description: 'The Real Estate Alliance League (REAL) is a community-driven real estate Decentralized Autonomous Organization (DAO) where members collaborate, learn, and contribute to the organization’s growth and vision.',
+    },
+    {
+      name: 'What makes REAL unique?',
+      description: 'REAL operates as a deflationary crypto utility token on the Ethereum blockchain, offering security and transparency through its network. There are no membership fees; holding REAL tokens automatically grants membership, with benefits increasing based on the number of tokens held.',
+    },
+    {
+      name: 'How does REAL operate?',
+      description: 'REAL focuses on acquiring and managing real estate rental properties. These properties provide ongoing support to the organization and its members through their continuous contribution to the ecosystem.',
+    },
+    {
+      name: 'What is Token Burn, and why is it important?',
+      description: 'REAL employs a process called "Token Burn," where tokens are purchased from the open market and permanently removed. This mechanism enhances the exclusivity of membership and aligns with the project’s long-term vision.',
+    },
+    {
+      name: 'Why should you choose REAL?',
+      description: 'REAL combines cutting-edge blockchain technology with a community-driven approach, creating an innovative platform backed by active participation and shared goals.',
+    },
+  ];
+
+  activeIndex: number | null = null;
+
+  toggleAccordion(index: number): void {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
 }
